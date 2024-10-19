2156a5b9e22c2b29dd89d72ae413e1f400dd3729
suvaisov <uvaissov@gmail.com>
2024-10-19 16:14:57 +0500

    service fix

--- deepface/service/src/modules/core/service.py
+++ deepface/service/src/modules/core/service.py
@@ -96,13 +96,11 @@ def find(
         total = len(dfs[0])
         fields = ["identity", "threshold", "distance"]
         results = []
+        # dns to text
 
-        # just conver dfs to str and return him
-        results.append(dfs[0].to_string())
-
-        # for i in range(total):
-        #     if i < 27 and all(i < len(dfs[0].get(field, [])) for field in fields):
-        #         results.append({field: dfs[0].get(field)[i] for field in fields})
+        for i in range(total):
+            if all(i < len(dfs[0].get(field)) for field in fields):
+                results.append({field: dfs[0].get(field)[i] for field in fields})
         return results
     except Exception as err:
         tb_str = traceback.format_exc()
